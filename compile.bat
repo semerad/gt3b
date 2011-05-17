7d6796876a6d0663bae95d9c24119ca58cc942a2
Pavel Semerad <semeradp@gmail.com>
2011-05-17 18:59:04 +0200

    make compile.bat working

--- compile.bat
+++ compile.bat
@@ -1,19 +1,22 @@
-make[1]: Entering directory `/home/semerad/c/stm8/GT3B'
-cxstm8 +warn +proto +mods0 +debug -i.  -l  -pxp -ac task.c
-cxstm8 +warn +proto +mods0 +debug -i.  -l  -pxp -ac main.c
-cxstm8 +warn +proto +mods0 +debug -i.  -l  -pxp -ac ppm.c
-cxstm8 +warn +proto +mods0 +debug -i.  -l  -pxp -ac lcd.c
-cxstm8 +warn +proto +mods0 +debug -i.  -l  -pxp -ac input.c
-cxstm8 +warn +proto +mods0 +debug -i.  -l  -pxp -ac buzzer.c
-cxstm8 +warn +proto +mods0 +debug -i.  -l  -pxp -ac timer.c
-cxstm8 +warn +proto +mods0 +debug -i.  -l  -pxp -ac eeprom.c
-cxstm8 +warn +proto +mods0 +debug -i.  -l  -pxp -ac config.c
-cxstm8 +warn +proto +mods0 +debug -i.  -l  -pxp -ac calc.c
-cxstm8 +warn +proto +mods0 +debug -i.  -l  -pxp -ac menu.c
-cxstm8 +warn +proto +mods0 +debug -i.  -l  -pxp -ac menu_service.c
-cxstm8 +warn +proto +mods0 +debug -i.  -l  -pxp -ac menu_global.c
-cxstm8 +warn +proto +mods0 +debug -i.  -l  -pxp -ac vector.c
-clnk  -o gt3b.sm8 -mgt3b.map compile.lkf
-cvdwarf gt3b.sm8
-chex -o gt3b.s19 gt3b.sm8
-make[1]: Leaving directory `/home/semerad/c/stm8/GT3B'
+@if "%TOOLSET%"=="" goto NoToolset
+%TOOLSET%/cxstm8 +warn +proto +mods0 +debug -i. -i%TOOLSET%/Hstm8 -l  -pxp -ac task.c
+%TOOLSET%/cxstm8 +warn +proto +mods0 +debug -i. -i%TOOLSET%/Hstm8 -l  -pxp -ac main.c
+%TOOLSET%/cxstm8 +warn +proto +mods0 +debug -i. -i%TOOLSET%/Hstm8 -l  -pxp -ac ppm.c
+%TOOLSET%/cxstm8 +warn +proto +mods0 +debug -i. -i%TOOLSET%/Hstm8 -l  -pxp -ac lcd.c
+%TOOLSET%/cxstm8 +warn +proto +mods0 +debug -i. -i%TOOLSET%/Hstm8 -l  -pxp -ac input.c
+%TOOLSET%/cxstm8 +warn +proto +mods0 +debug -i. -i%TOOLSET%/Hstm8 -l  -pxp -ac buzzer.c
+%TOOLSET%/cxstm8 +warn +proto +mods0 +debug -i. -i%TOOLSET%/Hstm8 -l  -pxp -ac timer.c
+%TOOLSET%/cxstm8 +warn +proto +mods0 +debug -i. -i%TOOLSET%/Hstm8 -l  -pxp -ac eeprom.c
+%TOOLSET%/cxstm8 +warn +proto +mods0 +debug -i. -i%TOOLSET%/Hstm8 -l  -pxp -ac config.c
+%TOOLSET%/cxstm8 +warn +proto +mods0 +debug -i. -i%TOOLSET%/Hstm8 -l  -pxp -ac calc.c
+%TOOLSET%/cxstm8 +warn +proto +mods0 +debug -i. -i%TOOLSET%/Hstm8 -l  -pxp -ac menu.c
+%TOOLSET%/cxstm8 +warn +proto +mods0 +debug -i. -i%TOOLSET%/Hstm8 -l  -pxp -ac menu_service.c
+%TOOLSET%/cxstm8 +warn +proto +mods0 +debug -i. -i%TOOLSET%/Hstm8 -l  -pxp -ac menu_global.c
+%TOOLSET%/cxstm8 +warn +proto +mods0 +debug -i. -i%TOOLSET%/Hstm8 -l  -pxp -ac vector.c
+%TOOLSET%/clnk -l%TOOLSET%/Lib -o gt3b.sm8 -mgt3b.map compile.lkf
+%TOOLSET%/cvdwarf gt3b.sm8
+%TOOLSET%/chex -o gt3b.s19 gt3b.sm8
+@goto:EOF
+
+:NoToolset
+@echo TOOLSET variable is not set, set it with "set TOOLSET=path"
